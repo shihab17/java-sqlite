59cc3d1c1c4ac256ccb7e9d1c2ccd798c98c4571
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-09-08 11:56:24 +0600

    insert and show

--- app/src/main/java/com/example/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.databasedemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    RadioGroup radioGroup;
+    RadioButton genderRB;
+    TextView txtName, txtAge;
+    Button btnSubmit, btnDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +28,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(this);
         SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
+
+        txtName = findViewById(R.id.name);
+        txtAge = findViewById(R.id.age);
+        radioGroup = findViewById(R.id.gender);
+        btnSubmit = findViewById(R.id.submit);
+        btnDisplay = findViewById(R.id.displayData);
+        btnSubmit.setOnClickListener(this);
+        btnDisplay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        if (view.getId() == R.id.submit) {
+            int selectedGender = radioGroup.getCheckedRadioButtonId();
+            genderRB = findViewById(selectedGender);
+            String name = txtName.getText().toString();
+            String age = txtAge.getText().toString();
+            String gender = genderRB.getText().toString();
+            MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(this);
+            long rowID = myDatabaseHelper.insertData(name, age, gender);
+            if (rowID == -1) {
+                Toast.makeText(MainActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Successfully Insert", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        if (view.getId() == R.id.displayData) {
+            Toast.makeText(this, "Display Data Clicked", Toast.LENGTH_SHORT).show();
+            MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(this);
+            Cursor cursor = myDatabaseHelper.displayAllData();
+
+            if (cursor.getCount() == 0) {
+                showData("Error", "No data Found ");
+            } else {
+                StringBuffer stringBuffer = new StringBuffer();
+                while (cursor.moveToNext()) {
+                    stringBuffer.append("ID " + cursor.getString(0));
+                    stringBuffer.append("\n Name " + cursor.getString(1));
+                    stringBuffer.append("\n Age " + cursor.getString(2));
+                    stringBuffer.append("\n Gender " + cursor.getString(3));
+
+                }
+                showData("Display Information", stringBuffer.toString());
+
+
+            }
+        }
+    }
+
+
+    public void showData(String title, String data) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setCancelable(true);
+        builder.setMessage(data);
+        builder.show();
     }
 }
--- app/src/main/java/com/example/databasedemo/MyDatabaseHelper.java
+++ app/src/main/java/com/example/databasedemo/MyDatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.databasedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -15,6 +17,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String GENDER = "_gender";
     private static final String TABLE_NAME = "students_details";
     private static final int VERSION_NUMBER = 1;
+    private static final String SELECT_ALL = "SELECT * FROM " + TABLE_NAME;
     private Context context;
 
     public MyDatabaseHelper(@Nullable Context context) {
@@ -25,23 +28,39 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         try {
-            Toast.makeText(context,"OnCreate is called: ",Toast.LENGTH_SHORT).show();
-            sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " VARCHAR (255), " + AGE + " INTEGER, "+GENDER+" VARCHAR(255)) ");
+            Toast.makeText(context, "OnCreate is called: ", Toast.LENGTH_SHORT).show();
+            sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " VARCHAR (255), " + AGE + " INTEGER, " + GENDER + " VARCHAR(255)) ");
         } catch (Exception e) {
-            Toast.makeText(context,"Exception: "+ e,Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Exception: " + e, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         try {
-            Toast.makeText(context,"onUpgrade is called: ",Toast.LENGTH_SHORT).show();
-            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME+"");
+            Toast.makeText(context, "onUpgrade is called: ", Toast.LENGTH_SHORT).show();
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + "");
             onCreate(sqLiteDatabase);
-        }
-        catch (Exception e){
-            Toast.makeText(context,"Exception: "+ e,Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(context, "Exception: " + e, Toast.LENGTH_SHORT).show();
         }
 
     }
+
+    public long insertData(String name, String age, String gender) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        contentValues.put(AGE, age);
+        contentValues.put(GENDER, gender);
+        long rowID = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+        return rowID;
+
+    }
+    public Cursor displayAllData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT  * FROM "+ TABLE_NAME + "",null);
+        return cursor;
+    }
+
 }
